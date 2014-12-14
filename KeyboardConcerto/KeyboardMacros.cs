ac52945a42fe5d3c51d6f82a44c629bcccf3ac1b
JesseZhong <jzz119@psu.edu>
2014-12-13 23:11:32 -0500

    Added unsynched files.

---
+++ KeyboardConcerto/KeyboardMacros.cs
@@ -0,0 +1,17 @@
+﻿// KeyboardMacros.cs
+// Authored by Jesse Z. Zhong
+#region Usings
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+#endregion
+
+namespace KeyboardConcerto {
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public class KeyboardMacros : Dictionary<Keys, KeyMacro> {
+
+	}
+}
---
+++ KeyboardConcerto/UserSettings.cs
@@ -0,0 +1,50 @@
+﻿// UserSettings.cs
+// Authored by Jesse Z. Zhong
+#region Usings
+using System;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using RawInput_dll;
+using BlockInput;
+#endregion
+
+namespace KeyboardConcerto {
+
+	/// <summary>
+	/// 
+	/// </summary>
+	[Serializable]
+	public class UserSettings {
+
+		#region Members
+		private Dictionary<string, KeyboardMacros> mKeyboardProfiles;
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// 
+		/// </summary>
+		public Dictionary<string, KeyboardMacros> KeyboardProfiles {
+			get {
+				return this.mKeyboardProfiles;
+			}
+			set {
+				this.mKeyboardProfiles = value;
+			}
+		}
+		#endregion
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public UserSettings() {
+			this.mKeyboardProfiles = new Dictionary<string, KeyboardMacros>();
+		}
+
+		public bool ProcessInput(InputEventArg arguments) {
+
+			return true;
+		}
+	}
+}

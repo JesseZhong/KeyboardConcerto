fc4f688888c211f83857af34f8bdf9f29cc8fe8f
JesseZhong <jzz119@psu.edu>
2014-12-22 04:41:01 -0500

    Message reading fully tested.

--- KeyboardConcerto/MainForm.cs
+++ KeyboardConcerto/MainForm.cs
@@ -17,21 +17,6 @@ using Common;
 namespace KeyboardConcerto {
 	public partial class MainForm : Form {
 
-		[DllImport("Interceptor.dll")]
-		private static extern bool InstallHook(IntPtr hWndParent);
-
-		[DllImport("Interceptor.dll")]
-		private static extern bool UninstallHook();
-
-		public struct HookParams {
-			public string State;
-			public string PrevState;
-			public string AltState;
-			public bool ExtendedKey;
-			public short ScanCode;
-			public short RepeatCount;
-		}
-
 		#region Constants
 		/// <summary>
 		/// The size of the memory-mapped file.
@@ -120,24 +105,8 @@ namespace KeyboardConcerto {
 				State = e.KeyPressEvent.KeyPressState,
 				Allow = this.mUserSettings.ProcessInput(e)
 			});
-
-			//this.TextBox.AppendText(String.Format("{0} {1} {2} {3} {4} {5}\n", dc.Key, dc.State, dc.Allow, silliness, stk.State, stk.PrevState));
-			this.TextBox.AppendText(String.Format("{0} {1} {2} {3}\n", dc.Key, dc.State, dc.Allow, sacred));
-
-			string name = "./output.txt";
-			if (!File.Exists(name)) {
-				using (File.Create(name)) {
-
-				}
-			}
-			using (StreamWriter file = new StreamWriter(name, true)) {
-				file.WriteLine(String.Format("{0} {1} {2} {3}\n", dc.Key, dc.State, dc.Allow, sacred));
-			}
 		}
 
-		uint silliness;
-		//HookParams stk;
-		string sacred;
 		/// <summary>
 		/// 
 		/// </summary>
@@ -154,15 +123,17 @@ namespace KeyboardConcerto {
 				case 32769: {
 						bool block = false;
 						bool decisionFound = false;
-						//Keys key = (Keys)Marshal.ReadInt32(msg.LParam);
-						//string state = (param.State & keyStateMask) == keyStateMask ? "BREAK" : "MAKE";
-						silliness = (uint)msg.WParam;
 						long lparam = (long)msg.LParam;
-						sacred = GetIntBinaryString(lparam);
-// 						stk = new HookParams() {
-// 							State = lparam >>
-// 						};
-// 
+						HookParams hParams = new HookParams() {
+							Key = (Keys)(uint)msg.WParam,
+							State = ((lparam >> 31 & 0x1) == 1) ? breakStr : makeStr,		// WM_DOWN is 0; WM_UP is 1
+							PrevState = ((lparam >> 30 & 0x1) == 1) ? makeStr : breakStr,	// WM_UP is 0; WM_DOWN is 0 (super confusing)
+							AltState = ((lparam >> 29 & 0x1) == 1) ? makeStr : breakStr,	// WM_UP is 0; WM_DOWN is 0
+							ExtendedKey = (lparam >> 24 & 0x1) == 1,
+							ScanCode = (byte)(lparam >> 16 & 0xF),
+							RepeatCount = (short)(lparam & 0xFF)
+						};
+
 // 						Stopwatch timer = new Stopwatch();
 // 						timer.Start();
 						//while (!decisionFound) {
@@ -198,23 +169,6 @@ namespace KeyboardConcerto {
 					}
 					break;
 			}
-		}
-
-		static string GetIntBinaryString(long n) {
-			char[] b = new char[64];
-			int pos = 63;
-			int i = 0;
-
-			while (i < 64) {
-				if ((n & (1 << i)) != 0) {
-					b[pos] = '1';
-				} else {
-					b[pos] = '0';
-				}
-				pos--;
-				i++;
-			}
-			return new string(b);
 		}
 		#endregion
 
@@ -262,6 +216,27 @@ namespace KeyboardConcerto {
 		#region Windows API
 		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
 		static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+		#endregion
+
+		#region Hooking
+		[DllImport("Interceptor.dll")]
+		private static extern bool InstallHook(IntPtr hWndParent);
+
+		[DllImport("Interceptor.dll")]
+		private static extern bool UninstallHook();
+
+		/// <summary>
+		/// Structure used to deserialize hooking messages for WH_KEYBOARD.
+		/// </summary>
+		private struct HookParams {
+			public Keys Key;
+			public string State;
+			public string PrevState;
+			public string AltState;
+			public bool ExtendedKey;
+			public byte ScanCode;
+			public short RepeatCount;
+		}
 		#endregion
 	}
 }

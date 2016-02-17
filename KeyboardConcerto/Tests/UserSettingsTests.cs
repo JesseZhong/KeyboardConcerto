328ff5e9af38f09c1e6f22125cae9d7fb3b2ab95
JesseZhong <jzz119@psu.edu>
2016-02-16 23:31:03 -0500

    Updated test to include the two desktop keyboards.

--- KeyboardConcerto/Tests/UserSettingsTests.cs
+++ KeyboardConcerto/Tests/UserSettingsTests.cs
@@ -19,9 +19,17 @@ namespace KeyboardConcerto.Tests {
 			execSeq.AddLast(new ExecVKey(WindowsInput.VirtualKeyCode.VK_S, ExecVKey.EState.Press));
 			execSeq.AddLast(new ExecVKey(WindowsInput.VirtualKeyCode.VK_T, ExecVKey.EState.Press));
 
+			// [Control] Fake keyboard input to function as the test control.
+			us.AddEntry(@"Fake Keyboard", RawInput.VirtualKeys.S, "MAKE", execSeq);
+
 			// For ASUS laptop's keyboard. 'A' key triggers macro sequence.
 			us.AddEntry(@"\\?\ACPI#PNP0303#4&16cfe3e0&0#{884b96c3-56ef-11d1-bc8c-00a0c91405dd}", RawInput.VirtualKeys.A, "MAKE", execSeq);
-			us.AddEntry(@"Fake Keyboard", RawInput.VirtualKeys.S, "MAKE", execSeq);
+
+			// For Logitech K200 Keyboard. '1' key triggers macro sequence.
+			us.AddEntry(@"\\?\HID#VID_046D&PID_C31D&MI_00#9&38b6029d&0&0000#{884b96c3-56ef-11d1-bc8c-00a0c91405dd}", RawInput.VirtualKeys.D1, "MAKE", execSeq);
+
+			// For Ducky Shine II. '2' key triggers macro sequence.
+			us.AddEntry(@"\\?\HID#VID_04D9&PID_0183&MI_00#8&6d34b6b&0&0000#{884b96c3-56ef-11d1-bc8c-00a0c91405dd}", RawInput.VirtualKeys.D2, "MAKE", execSeq);
 
 			return us;
 		}
